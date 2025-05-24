4a45d1cbb7ecdab6718e64f3239c6c93c03a8360
Matthew McDermott <mattmcdermott8@gmail.com>
2025-05-24 14:57:32 -0400

    Fixed some typos and such

--- src/MEDS_visualizations/plotters/density_heatmap.py
+++ src/MEDS_visualizations/plotters/density_heatmap.py
@@ -1,21 +1,31 @@
 import plotly.graph_objects as go
 import polars as pl
+from typing import Sequence, Any
+from datetime import timedelta
 
 from .base import BasePlotter
 
+SECONDS_IN_DAY = 60*60*24
+
 
 class DensityHeatmap(BasePlotter[pl.DataFrame, go.Figure]):
-    def __init__(self, x: str, y: str, z: str = "n_measurements"):
+    def __init__(self, x: str, y: str):
         self.x = x
         self.y = y
-        self.z = z
+
+    def normalize_plot_data(self, arr: Sequence[Any]):
+        if isinstance(arr[0], timedelta):
+            arr = [td.total_seconds() / SECONDS_IN_DAY for td in arr]
+        return arr
 
     def render(self, plot_data: pl.DataFrame) -> go.Figure:
+        x = self.normalize_plot_data(plot_data[self.x].to_list())
+        y = self.normalize_plot_data(plot_data[self.y].to_list())
+
         return go.Figure(
             data=go.Histogram2d(
-                x=plot_data[self.x].to_list(),
-                y=plot_data[self.y].to_list(),
-                z=plot_data[self.z].to_list(),
+                x=x,
+                y=x,
                 coloraxis="coloraxis",
             )
         ).update_layout(coloraxis={"colorscale": "Viridis"}, title="2D Measurement Density")
